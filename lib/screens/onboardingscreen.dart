16f30e9ed1be32f9b4c8f402efc4ce585ffb8e67
sachinNishalka <nishalkasachin@gmail.com>
2024-02-28 16:02:34 +0530

    compleated the onboard screen with stateless widgets

--- lib/screens/onboardingscreen.dart
+++ lib/screens/onboardingscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:dimekeeper/data/onboardingcontent.dart';
 
@@ -11,26 +12,77 @@ class OnBoardinScreen extends StatefulWidget {
 
 class _OnBoardinScreenState extends State<OnBoardinScreen> {
   late List<OnboardingCotent> content;
+  int currentIndex = 0;
+  late PageController _controller;
 
   @override
   void initState() {
     content = OnboardingCotent.getContent();
+    _controller = PageController(initialPage: 0);
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFDFF8FF),
-      body: PageView.builder(
-        itemCount: content.length,
-        itemBuilder: (_, i) {
-          return SafeArea(
-            child: Column(
-              children: [SvgPicture.asset(content[i].image)],
-            ),
-          );
-        },
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  controller: _controller,
+                  onPageChanged: (int index) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                  itemCount: content.length,
+                  itemBuilder: (_, i) {
+                    return Column(
+                      children: [SvgPicture.asset(content[i].image)],
+                    );
+                  },
+                ),
+              ),
+              Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFF125A93),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: TextButton(
+                  child: Text(
+                    currentIndex == content.length - 1 ? "Get Started" : "Next",
+                    style: TextStyle(color: Color(0xFFDFF8FF), fontSize: 14.0),
+                  ),
+                  onPressed: () {
+                    if (currentIndex == content.length - 1) {}
+                    _controller.nextPage(
+                        duration: Duration(milliseconds: 100),
+                        curve: Curves.bounceIn);
+                  },
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  currentIndex == content.length - 1 ? "" : "Skip",
+                  style: TextStyle(color: Color(0xFF125A93), fontSize: 14.0),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
